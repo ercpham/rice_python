be57f7764095195d73b9623dd78222eecc208f0a
ercpham <erc.pham@gmail.com>
2020-07-01 11:34:10 -0700

    Changed functionality to allow for multiple dice of different sizes

--- dice_roll/dice_roll.py
+++ dice_roll/dice_roll.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python3
 import random
-min = 1
-max = 6
 
-print(random.randint(min, max))
+size_string = input("Enter dice sizes: ")
+size_list = size_string.split(" ")
+size_list = list(map(int, size_list))
+
+roll_sum = 0
+
+length = len(size_list)
+
+for index in range(length):
+    roll = random.randint(1, size_list[index])
+    print("Dice {}: {}".format(index+1, roll))
+    roll_sum += roll
+
+print("Sum of dice rolls: {}".format(roll_sum))
