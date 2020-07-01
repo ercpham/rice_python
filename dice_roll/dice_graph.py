7bea5b6107d0c41c44858e356242843db024a02a
ercpham <erc.pham@gmail.com>
2020-07-01 14:22:56 -0700

    Rolling multiple dice of varied size

--- dice_roll/dice_graph.py
+++ dice_roll/dice_graph.py
@@ -5,11 +5,19 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+size_string = input("Enter dice sizes: ")
+size_list = size_string.split(" ")
+size_list = list(map(int, size_list))
+length = len(size_list)
+
 num_rolls = int(input("Enter number of times to roll: "))
 roll_list = []
 
 for i in range(num_rolls):
-    roll_list.append(random.randint(1,6) + random.randint(1,6))
+    roll_sum = 0
+    for j in range(length):
+        roll_sum += random.randint(1,size_list[j])
+    roll_list.append(roll_sum)
 
 df = pd.DataFrame(data = {'Roll Sums': roll_list})
 
